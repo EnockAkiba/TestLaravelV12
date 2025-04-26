5a34e36ac1f601c2c30758981cfc2e23c400c85a
paulbebukya <wise10paul@gmail.com>
2025-04-26 19:08:22 +0200

    menu-toggle

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,5 @@
 <x-layouts.app :title="__('Dashboard')">
-    <div class="d-flex justify-content-between align-items-center">
-        <h1>
-            <button class="btn bg-dark p-1 d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#menu"
-                aria-controls="menu">
-                <i class="fas fa-bars"></i>
-            </button>
-            <button class="btn bg-dark p-1 d-none d-lg-inline-block menu-toggle" type="button">
-                <i class="fas fa-bars"></i>
-            </button>
-            TABLEAU DE BORD
-        </h1>
-        <input type="search" wire:model.live="search" class="w-25 mb-2" placeholder="Recherche ...">
-    </div>
-
+ 
     <div class="content-main">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi mollitia dicta tempore, accusamus adipisci, facilis saepe at eveniet atque, tempora unde.
     </div>
